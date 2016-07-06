4609332533b0e288109cb722974f5d3fd7e516bf
Oleg Varaksin <ovaraksin@googlemail.com>
2016-07-07 01:03:03 +0200

    Tests

--- 2-gulp/app/js/main.spec.ts
+++ 2-gulp/app/js/main.spec.ts
@@ -1,38 +1,31 @@
 /// <reference path="../../typings/index.d.ts" />
 
-declare function require(moduleName: string): any;
+declare function require(moduleName:string):any;
 
-import {join} from "path";
-import {env, Callback} from "jsdom";
 import {expect} from "chai";
-import * as fs from "fs";
 import {Application as App} from "./main";
 
 describe('Application tests', function () {
-    const html = fs.readFileSync(join(__dirname, 'index.html'), 'utf8');
-    let app : App;
+    let app:App;
+    let cleanup:Function;
 
-    /*
-    beforeEach(function () {
-        app = new Application(".greeting");
+    beforeEach(function () {      
+        // setup the simplest document and window
+        cleanup = require("jsdom-global")();
     });
-    */
+    
+    afterEach(function () {
+        cleanup();
+    });
 
     it('check innerHTML', (done) => {
-        env({
-            html: html,
-            src: [],
-            done: (err, window) => {
-                let Application: typeof App = require("./main");
-                app = new Application('.greeting');
-                app.showHello('TypeScript!');
-                
-                const el = <HTMLElement>document.querySelectorAll('.greeting')[0];
-                expect(el.innerHTML).to.be.eq('TypeScript!');
-    
-                window.close();
-                done();
-            }
-        });
+        let Application:typeof App = require("./main");
+        app = new Application('.greeting');
+        app.showHello('TypeScript!');
+
+        const el = <HTMLElement>document.querySelectorAll('.greeting')[0];
+        expect(el.innerHTML).to.be.eq('TypeScript!');
+        
+        done();
     })
 });
