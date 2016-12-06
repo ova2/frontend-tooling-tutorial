603f0e91dc22a4e1c6c508e70cdcce43e3ffa1a9
Oleg Varaksin <ovaraksin@googlemail.com>
2016-12-06 23:20:32 +0100

    More implementation + first tests with karma and jasmine

---
+++ typescript-playground/module-augmentation/karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ["jasmine", "karma-typescript"],
+
+        files: [
+            {pattern: "src/**/*.ts"}
+        ],
+
+        preprocessors: {
+            "**/*.ts": ["karma-typescript"]
+        },
+
+        reporters: ["mocha", "karma-typescript"],
+
+        browsers: ["PhantomJS"],
+
+        phantomJsLauncher: {
+            exitOnResourceError: true
+        },
+
+        port: 9876,
+
+        // Uncomment below if you want the default html
+        // coverage report + a summary on the console
+        /*
+         karmaTypescriptConfig: {
+         reports:
+         {
+         "html": "coverage",
+         "text-summary": "" // destination "" will redirect output to the console
+         }
+         },
+         //*/
+
+        // Uncomment below if you want to disable code coverage
+        // instrumentation during debugging of tests
+        /*
+         karmaTypescriptConfig: {
+         disableCodeCoverageInstrumentation: true
+         },
+         //*/
+    });
+};
